bd019ed193bfca06c37a00abb5f942878e9ec996
Tim Turner <timdturner@gmail.com>
2016-10-27 20:23:17 -0400

    Add WishlistController; update Library class

---
+++ src/Silverscreen.Core/Controllers/WishlistController.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Silverscreen.Model;
+
+namespace Silverscreen.Core.Controllers {
+    [Route("api/[controller]")]
+    public class WishlistController : Controller
+    {
+        
+    }
+
+}
--- src/Silverscreen.Library/Library.cs
+++ src/Silverscreen.Library/Library.cs
@@ -4,11 +4,11 @@ using Silverscreen.Model;
 namespace Silverscreen.Library {
     public class Library : IEntityBase {
         public Library () {
-            List<string> Paths = new List<string>();
+            List<string> Directories = new List<string>();
             List<Movie> Movies = new List<Movie>();
         }
         public int Id { get; set; }
-        public List<string> Paths { get; set; }
+        public List<string> Directories { get; set; }
         public List<Movie> Movies { get; set;}
     }
 }
