d159d717b513357dc0bd3ce24c11b4aed7cfe004
Tim Turner <timdturner@gmail.com>
2016-11-06 09:35:29 -0500

    Get movie by id on controller

--- src/Silverscreen.Core/Controllers/LibraryController.cs
+++ src/Silverscreen.Core/Controllers/LibraryController.cs
@@ -28,15 +28,7 @@ namespace Silverscreen.Core.Controllers {
         [HttpGet("/movies/{id}")]
         public IActionResult GetMovie(int id)
         {
-            //Movie _movie = _movieRepository.GetSingle(m => m.Id == id);
-            Movie _movie = new Movie() {
-                Id = 1,
-                Title = "Contact",
-                Plot = "Dr. Ellie Arroway, after years of searching, finds conclusive radio proof of intelligent aliens, who send plans for a mysterious machine.",
-                ImdbId = "tt0118884",
-                Year = 1997,
-                Poster = "https://images-na.ssl-images-amazon.com/images/M/MV5BYWNkYmFiZjUtYmI3Ni00NzIwLTkxZjktN2ZkMjdhMzlkMDc3XkEyXkFqcGdeQXVyNDk3NzU2MTQ@._V1_UX182_CR0,0,182,268_AL_.jpg"
-            };
+            Movie _movie = _service.GetMovie(id);
 
             if(_movie != null) 
             {
